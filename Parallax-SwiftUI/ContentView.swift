7bac3e77a93cea404b49d31766ad1dd7b19181b9
Kevin Ahmad <kevinahmad.f@gmail.com>
2022-07-16 18:04:59 +0700

    Initial Commit

---
+++ Parallax-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Parallax-SwiftUI
+//
+//  Created by Kevin ahmad on 16/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Parallax-SwiftUI/Parallax_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Parallax_SwiftUIApp.swift
+//  Parallax-SwiftUI
+//
+//  Created by Kevin ahmad on 16/07/22.
+//
+
+import SwiftUI
+
+@main
+struct Parallax_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
